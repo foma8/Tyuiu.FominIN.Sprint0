3452e01234817740d0983a1467604bd4c783fc0d
Tyuiu.FominVN@outlook.com <hialdez111@gmail.com>
2023-09-20 11:06:46 +0500

    Add project files.

---
+++ Tyuiu.FominIN.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.FominIN.Sprint0.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
