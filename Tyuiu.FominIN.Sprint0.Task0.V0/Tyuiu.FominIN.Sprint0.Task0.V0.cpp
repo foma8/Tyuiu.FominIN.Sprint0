92dda2835378339030ba1fe543daac9ac6b62a86
Tyuiu.FominVN@outlook.com <hialdez111@gmail.com>
2023-09-20 20:42:42 +0500

    Task1

--- Tyuiu.FominIN.Sprint0.Task0.V0/Tyuiu.FominIN.Sprint0.Task0.V0.cpp
+++ Tyuiu.FominIN.Sprint0.Task0.V0/Tyuiu.FominIN.Sprint0.Task0.V0.cpp
@@ -1,19 +1,11 @@
-// Tyuiu.FominIN.Sprint0.Task1.V0.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Tyuiu.FominIN.Sprint0.Task0.V0.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
-using namespace std;
+
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    cout << "Hello World!\n";
-    cout << "Ввидите ФИО : ";
-    string a;
-    cin >> a;
-    int v;
-    cin >> v;
-    cout << "Возраст - " << v;
-    return 0;
+    std::cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
